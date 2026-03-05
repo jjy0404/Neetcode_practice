cb7ef54b46cc78e76274dcacb9c54d7ad84d5996
jjy0404 <jiyongjung04@gmail.com>
2026-03-05 19:48:29 +0000

    05/03/2026

---
+++ Neetcode150/Arrays _and_Hashings/GroupAnagram.py
@@ -0,0 +1,29 @@
+# Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+# Example 1:
+
+# Input: strs = ["act","pots","tops","cat","stop","hat"]
+
+# Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+# Example 2:
+
+# Input: strs = ["x"]
+
+# Output: [["x"]]
+# Example 3:
+
+# Input: strs = [""]
+
+# Output: [[""]]
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ref = defaultdict(list);
+        for s in strs:
+            count = [0] * 26;
+            for c in s:
+                count[ord(c) - ord('a')] += 1;
+            ref[tuple(count)].append(s);
+        return list(ref.values());
